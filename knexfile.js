7f182ad20fb56921e3d05121e85b69fde41954c3
BaileyLeong <baileyleong@gmail.com>
2025-02-24 14:58:10 -0500

    merge "develop" into main

--- knexfile.js
+++ knexfile.js
@@ -3,16 +3,10 @@ import "dotenv/config";
 export default {
   client: "mysql2",
   connection: {
-    host: process.env.DB_HOST,
-    database: process.env.DB_DATABASE,
-    user: process.env.DB_USER,
-    password: process.env.DB_PASSWORD,
+    host: process.env.DB_LOCAL_HOST,
+    database: process.env.DB_LOCAL_DBNAME,
+    user: process.env.DB_LOCAL_USER,
+    password: process.env.DB_LOCAL_PASSWORD,
     charset: "utf8",
-  },
-  pool: {
-    min: 2,
-    max: 5,
-    acquireTimeoutMillis: 30000,
-    idleTimeoutMillis: 10000,
   },
 };
